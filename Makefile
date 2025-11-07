ce285f8ada3f9d20bccd043e796987fa11f95747
Loena Nascimento Da Silva <loda-sil@c2r8p3.42sp.org.br>
2025-11-07 18:26:06 -0300

    feat: add ft_printf

--- Makefile
+++ Makefile
@@ -8,27 +8,32 @@ GNL_SRCS = lib/get_next_line/get_next_line.c \
 OBJS = $(SRCS:.c=.o) $(GNL_SRCS:.c=.o)
 
 CC = cc
-CFLAGS = -Wall -Werror -Wextra -Iincludes -Ilib/minilibx-linux -Ilib/get_next_line -Ilib/libft
+CFLAGS = -Wall -Werror -Wextra -Iincludes -Ilib/minilibx-linux -Ilib/get_next_line -Ilib/libft -Ilib/ft_printf
 
 MLX = -Llib/minilibx-linux -lmlx -L/usr/lib -lXext -lX11 -lm -lz
 LIBFT = lib/libft/libft.a
+LIBFTPRINTF = lib/ft_printf/libftprintf.a
 
 all: $(NAME)
 
 $(NAME): $(OBJS)
 	make -C lib/minilibx-linux
 	make -C lib/libft
-	$(CC) $(OBJS) $(LIBFT) $(MLX) -o $(NAME)
+	make -C lib/ft_printf
+	$(CC) $(OBJS) $(LIBFT) $(LIBFTPRINTF) $(MLX) -o $(NAME)
 
 clean:
 	rm -f $(OBJS)
 	make clean -C lib/minilibx-linux
 	make clean -C lib/libft
+	make clean -C lib/ft_printf
+
 
 fclean: clean
 	rm -f $(NAME)
 	make clean -C lib/minilibx-linux
 	make fclean -C lib/libft
+	make fclean -C lib/ft_printf
 
 re: fclean all
 
---
+++ lib/ft_printf/ft_printf.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: loda-sil <loda-sil@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/18 18:12:09 by loena             #+#    #+#             */
+/*   Updated: 2025/09/03 12:19:44 by loda-sil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <unistd.h>
+
+int	ft_printf(const char *format, ...);
+int	ft_putchar(char c);
+int	ft_putstr(char *s);
+int	ft_putnbr(int n);
+int	ft_putnbr_unsigned(unsigned int n);
+int	ft_puthexa(unsigned long int n, const char *base);
+
+#endif
