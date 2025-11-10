8bc68347e7a2fa6a8776d943aa56d266a75852d9
Loena Nascimento Da Silva <loda-sil@c2r5p2.42sp.org.br>
2025-11-10 18:39:51 -0300

    feat: add error file

--- src/utils/ft_error.c
+++ src/utils/ft_error.c
@@ -6,14 +6,15 @@
 /*   By: loda-sil <loda-sil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/07 18:05:55 by loda-sil          #+#    #+#             */
-/*   Updated: 2025/11/07 19:13:16 by loda-sil         ###   ########.fr       */
+/*   Updated: 2025/11/10 17:05:21 by loda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/so_long.h"
 
-static int	message_error(char *message)
+int	message_error(char *message)
 {
-	ft_printf("Error\n%s\n", message);
+	ft_printf("Error: %s\n", message);
 	return (0);
 }
+
