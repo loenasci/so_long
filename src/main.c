b62384f6ceead040c35b4b8a0b07cf636312c6e7
Loena Nascimento Da Silva <loda-sil@c2r5p2.42sp.org.br>
2025-11-11 14:13:50 -0300

    feat: add functions to check the map

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: loda-sil <loda-sil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/21 17:59:27 by loda-sil          #+#    #+#             */
-/*   Updated: 2025/11/10 18:34:08 by loda-sil         ###   ########.fr       */
+/*   Updated: 2025/11/11 14:05:11 by loda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,12 @@ int	main(int argc, char *argv[])
 	t_game game;
 	
 	if (argc != 2)
-		return (message_error("invalid number of parameters!\n Use: ./program <map_path>"));
+		return (exit_error("invalid number of parameters!\n Use: ./program <map_path>"));
 	if (!map_check_extension(argv[1], ".ber"))
-		return (message_error("Invalid file extension! Use .ber"));
+		return (exit_error("Invalid file extension! Use .ber"));
 	if (!map_read(&game, argv[1]))
-		return (1);
+		return (exit_error("Invalid map!"));
+	if (!map_check(&game))
+		return (exit_error("Invalid map!"));
 	return (0);
 }
