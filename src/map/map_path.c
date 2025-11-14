aa2abcddb8730e3a8fe9448abe234bb94206372f
Loena Nascimento <loenascimentosilva@gmail.com>
2025-11-14 00:04:05 -0300

    refactor: add functions prototype

--- src/map/map_path.c
+++ src/map/map_path.c
@@ -6,15 +6,23 @@
 /*   By: loena <loena@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/13 23:23:53 by loena             #+#    #+#             */
-/*   Updated: 2025/11/13 23:41:04 by loena            ###   ########.fr       */
+/*   Updated: 2025/11/14 00:03:45 by loena            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/so_long.h"
 
 static char **map_copy_layout(char **layout)
+{
+	
+}
 
-int	**map_fill_valid_paths()
+static void	**map_fill_valid_paths()
+{
+	
+}
+
+int map_has_valid_path(t_game *game)
 {
 	
 }
